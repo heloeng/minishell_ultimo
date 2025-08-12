ba8550e2d057a564bcedf0b44ef060d840538097
Heloiza Mendes <helocursos@gmail.com>
2025-08-11 23:43:25 -0300

    refatoração

--- m/configure_signals.c
+++ m/configure_signals.c
@@ -1,19 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   configure_signals.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dydaniel <dydaniel@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 23:01:49 by dydaniel          #+#    #+#             */
+/*   Updated: 2025/08/11 23:02:24 by dydaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-void handle_ctrlc(int sig)
+void	handle_ctrlc(int sig)
 {
-    
-    (void)sig;
-
-    write(STDOUT_FILENO, "\n", 1);
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-void configure_signal()
+void	configure_signal()
 {
-    signal(SIGINT, handle_ctrlc);
-    signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, handle_ctrlc);
+	signal(SIGQUIT, SIG_IGN);
 }
