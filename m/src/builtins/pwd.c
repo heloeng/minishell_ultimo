07c987081f7f0df5068f04b3c909d128b2f61dff
Heloiza Mendes <helocursos@gmail.com>
2025-08-14 19:32:46 -0300

    pwd norminette

--- m/src/builtins/pwd.c
+++ m/src/builtins/pwd.c
@@ -1,15 +1,16 @@
 
 #include "minishell.h"
 
-void ft_pwd(void)
+void	ft_pwd(void)
 {
-    char cwd[1024];
-    if(getcwd(cwd, sizeof(cwd)) == NULL)
-    {
-        ft_printf("pwd: error retrieving current directory: getcwd: \
-            cannot access parent directories: No such file or directory\n");
-        return;
-    }
-    ft_putstr_fd(cwd, 1);
-    ft_putstr_fd("\n", 1);
+	char	cwd[1024];
+
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		ft_printf("pwd: error retrieving current directory: getcwd: \
+				cannot access parent directories: No such file or directory\n");
+		return ;
+	}
+	ft_putstr_fd(cwd, 1);
+	ft_putstr_fd("\n", 1);
 }
