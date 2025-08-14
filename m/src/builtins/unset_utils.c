d296973e5962af766d426a608557a8cee2f87610
Heloiza Mendes <helocursos@gmail.com>
2025-08-14 19:21:44 -0300

    unset_utils norminette

--- m/src/builtins/unset_utils.c
+++ m/src/builtins/unset_utils.c
@@ -1,41 +1,41 @@
 
 #include "minishell.h"
 
-
-int ft_unset_args(char **args, t_data_val *data)
+int	ft_unset_args(char **args, t_data_val *data)
 {
-	int i = 1;
+	int	i;
 
-	while (args[i]) 
+	i = 1;
+	while (args[i])
 	{
-		ft_unset(&data->envp, args[i]); 
+		ft_unset(&data->envp, args[i]);
 		i++;
 	}
-	return (0); 
+	return (0);
 }
 
-char **build_new_env(char **old, const char *name)
+char	**build_new_env(char **old, const char *name)
 {
-	char **new;
-	int i;
-	int j;
-	int len;
+	char	**new;
+	int		i;
+	int		j;
+	int		len;
 
 	i = 0;
 	j = 0;
 	len = ft_strlen(name);
 	while (old[i])
 		i++;
-	new = malloc(sizeof(char *) * (i + 1)); 
+	new = malloc(sizeof(char *) * (i + 1));
 	if (!new)
-		return NULL;
+		return (NULL);
 	i = 0;
-	while(old[i])
+	while (old[i])
 	{
-		if(!(ft_strncmp(old[i], name, len) == 0 && old[i][len] == '='))
+		if (!(ft_strncmp(old[i], name, len) == 0 && old[i][len] == '='))
 			new[j++] = ft_strdup(old[i]);
 		i++;
 	}
 	new[j] = NULL;
-	return new;
+	return (new);
 }
